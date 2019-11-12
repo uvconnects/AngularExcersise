46b8059aae52a87bed5d35ce7d81e80aae3d67bb
Anthony Price <sevprice@gmail.com>
2019-11-11 22:08:56 -0500

    animations part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,17 +5,31 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dishfeedform: FormGroup;
   errorMsg: string;
   dishErrorMsg: string;
-  @ViewChild('slider', {static: true}) slider;
+  visibility='shown';
+  @ViewChild('slider', { static: true }) slider;
   @ViewChild('fform', { static: false }) feedbackFormDirect: any;
   formErrors = {
     'author': '',
@@ -41,7 +55,7 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
-    private fb: FormBuilder,  @Inject('baseURL') private baseURL) {
+    private fb: FormBuilder, @Inject('baseURL') private baseURL) {
     this.createForm();
   }
   createForm() {
@@ -79,10 +93,9 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds()
       .subscribe((dishIds) => this.dishIds = dishIds);
 
-    this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id) },
-      errorMsg => this.errorMsg = <any>errorMsg);
+      this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+        errmess => this.errorMsg = <any>errmess);
 
   }
   formatLabel(value: number) {
@@ -104,13 +117,13 @@ export class DishdetailComponent implements OnInit {
     this.dishcopy.comments.push(this.dishfeedform.value);
     this.dishservice.putDish(this.dishcopy).subscribe(dish => {
       this.dish = dish; this.dishcopy = dish;
-      errorMsg  => {this.dish = null; this.dishcopy = null; this.errorMsg = <any>errorMsg; }
+      errorMsg => { this.dish = null; this.dishcopy = null; this.errorMsg = <any>errorMsg; }
     });
     this.dishfeedform.reset({
       author: '',
       comment: ''
-    }); 
-   
+    });
+
     this.feedbackFormDirect.resetForm();
     this.resetvalue = 5;
   }
